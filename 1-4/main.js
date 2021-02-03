7afdfc8b9f0b7f87aaa88327f8cbccbfa9177de5
fukufukufukurou <78461607+fukufukufukurou@users.noreply.github.com>
2021-02-03 22:09:17 +0900

    課題提出（1-4）
    1-4の課題

---
+++ 1-4/main.js
@@ -0,0 +1,11 @@
+for(let number=0 ; number<=100 ; number++){
+  if(number%3===0){
+    console.log("Fizz!");
+  }else if(number%5===0){
+    console.log("Buzz!");
+  }else if(number%3===0 && number%5===0){
+    console.log("FizzBuzz!!");
+  }else{
+    console.log(number);
+  }
+}
