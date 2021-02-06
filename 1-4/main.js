50bcf412ef6678ceec9c3eac2aaba92e485a3029
fukufukufukurou <78461607+fukufukufukurou@users.noreply.github.com>
2021-02-07 05:45:55 +0900

    課題再提出(1-4)
    ifとelse if とelse

--- 1-4/main.js
+++ 1-4/main.js
@@ -1,10 +1,10 @@
 for(let number=0 ; number<=100 ; number++){
-  if(number%3===0){
-    console.log("Fizz!");
+  if(number%3===0 && number%5===0){
+    console.log("FizzBuzz!");
   }else if(number%5===0){
     console.log("Buzz!");
-  }else if(number%3===0 && number%5===0){
-    console.log("FizzBuzz!!");
+  }else if(number%3===0){
+    console.log("Fizz!");
   }else{
     console.log(number);
   }
